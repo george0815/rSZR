37e36cb1db771d4d74629f384afa23b1b2c30188
hunters11 <shipmang8@gmail.com>
2021-01-01 00:27:34 -0500

    Added more formats, repositioned some of the if statements
    Added more image formats and made it so it checks if there are images in the folder before it tries to resize.

--- gw.cpp
+++ gw.cpp
@@ -6,6 +6,9 @@
 #include <QDebug>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <set>
+#include <stdio.h>
+#include <ctype.h>
 
 
 
@@ -34,18 +37,51 @@ void GW::on_cropResize_clicked()
 
 
 
+    //image formats
+    set<string> formats =             {"png",
+                                       "jfif",
+                                       "jpg",
+                                       "jpeg",
+                                       "jpe",
+                                       "raw",
+                                       "jif",
+                                       "dds",
+                                       "jfi",
+                                       "gif",
+                                       "webp",
+                                       "bmp",
+                                       "tif",
+                                       "tga",
+                                       "tiff",
+                                       "arw",
+                                       "cr2",
+                                       "nrw,"
+                                       "k25",
+                                      "dib",
+                                       "heif",
+                                      "heic",
+                                      "ind",
+                                      "indd",
+                                      "indt",
+                                      "jp2",
+                                      "j2k",
+                                      "jpf",
+                                      "jpx",
+                                      "jpm",
+                                      "mj2"};
+
+
+    //open file dialog so user caan choose img folder
     QFileDialog dialog;
 
     dialog.setFileMode(QFileDialog::Directory);
     dialog.setOption(QFileDialog::ShowDirsOnly);
 
+    QString path = dialog.getExistingDirectory(this, "Open Image Folder");
 
 
-    QString path = dialog.getExistingDirectory(this, "Open Sekiro Folder");
 
-
-    QString imgDir = path;
-    QDir dir(imgDir);
+    QDir dir(path);
 
     QDir().mkdir(".\\Resized\\");
 
@@ -55,23 +91,52 @@ void GW::on_cropResize_clicked()
 
 
 
-
+    //for counting how many files are valid(images)
     int i = 0;
 
 
-    //for eeach image in folderr, resize it
+
+    //if the folder chosen is valid, proceed with the resizing, if not then throw error
+    if(!path.isEmpty()&& !path.isNull()){
+
+
+
+
+        //checks how many valid files there are in the folder
+        foreach (QFileInfo fileInfo, dir.entryInfoList()) {
+
+            string lower = fileInfo.suffix().toLocal8Bit().constData();
+
+            std::transform(lower.begin(), lower.end(), lower.begin(), std::tolower);
+
+            if(formats.find(lower) != formats.end()){
+               i++;
+            }
+
+        }
+
+
+    //if there are valid files, proceed, if not, then throw error
+    if(i > 0){
+
+    //for each image in folder, resize it
+
     foreach (QFileInfo fileInfo, dir.entryInfoList()) {
 
-
-
-
-
-        if(fileInfo.suffix() == "png" || fileInfo.suffix() == "jpg" || fileInfo.suffix() == "jpeg"){
             i++;
+
+            string lower = fileInfo.suffix().toLocal8Bit().constData();
+
+            std::transform(lower.begin(), lower.end(), lower.begin(), std::tolower);
+
+
+
+            if(formats.find(lower) != formats.end()){
 
             QImage img(fileInfo.filePath());
 
             QPixmap pixmap;
+
             pixmap = pixmap.fromImage(img.scaled(ui->Width->value(), ui->Height->value(),Qt::IgnoreAspectRatio,Qt::SmoothTransformation));
 
             QString final = ("./Resized/" + fileInfo.fileName());
@@ -82,20 +147,31 @@ void GW::on_cropResize_clicked()
             pixmap.save(&file, "jpeg",100);
             file.close();
 
+            }
 
         }
 
+     }
 
+
+    else{
+            QMessageBox::information(this, "Error", "No valid images in folder.");
+        }
 
     }
 
-    //checks if folder had images in it
+    else{
+        QMessageBox::information(this, "Error", "No folder chosen.");
+    }
+
+
+
+
+    //if amount of valid images insside ressized folder is greater then one, then throw confirmation message
     if(i > 0){
         QMessageBox::information(this, "Finished", "Resizing process done.");
     }
-    else{
-         QMessageBox::information(this, "Error", "No valid images in folder.");
-    }
+
 
 
 
